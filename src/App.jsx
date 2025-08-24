6a8b2f473e09e90dd2cefe59b697e87520373a7b
shrikaranks-786 <shrikaranksise2024@gmail.com>
2025-08-24 16:54:25 +0530

    created authpage inital setup and installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div>hello</div>;
+  return <div className="">
+
+  </div>;
 }
 
 export default App;
---
+++ src/pages/Authpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Authpage() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Authpage
